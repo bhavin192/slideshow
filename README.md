63e30ed441d8560f298962358500e61a551a0c08
user <user@localhost.localdomain>
2018-05-03 02:38:33 +1000

    Message

--- README.md
+++ README.md
@@ -1,19 +1,13 @@
-# slideshow
+# Slideshow (alpha development phase, testing on Fedora 27 (tm) ).
 Slideshow and individual image viewer, with sortable and zoom facility.
 
 ## Config
 
-In index.js edit:
-
-var slidesDir = "/home/user/slideshow/slides/";
-
-To the directory of your images.
-
-Then run:
+To run example:
 
 node index.js
 
-Where index.js resides.
+From where index.js resides.
 
 Then open URI of index.html in browser, eg:
 
@@ -21,4 +15,22 @@ file:///home/user/slideshow/index.html
 
 NB not URL http://...
 
+To display your images, edit index.js:
+
+var slidesDir = "/home/user/slideshow/slides/";
+
+To the directory of your images, or simply add/replace images in the slides/ sub-directory.
+
+## HowTo
+
+Drag sortable thumbnails.
+
+Click play icon.
+
+To view indvidual images, click on thumb or on image while playing or pause icon.
+
+Click play icon to resume.
+
+Click stop icon to return to sortable thumbs.
+		
 
---
+++ index.js
@@ -0,0 +1,36 @@
+var io = require('socket.io').listen(8000);
+var fs = require('fs');
+var path = require("path");
+var slidesObj = [];
+
+var slidesDir = "/home/user/slideshow/slides/";
+
+function readImg() {
+  var files = fs.readdirSync(slidesDir);
+  slidesObj = [];
+  files.forEach(function(file) {
+    let fileStat = fs.statSync(slidesDir + file).isDirectory();
+
+    if (!fileStat) {
+      var extName = path.extname(file).toLowerCase();
+      var hex = new Buffer(file).toString('hex');
+      console.log("file : " + JSON.stringify(file, null, 2));
+      var key = hex;
+      var data = {
+        uri: file,
+        uriKey: key
+      };
+      if (extName == ".png" || extName == ".jpg" || extName == ".jpeg" || extName == ".svg" || extName == ".bmp" || extName == ".gif") {
+        slidesObj.push(data);
+      }
+    }
+  });
+}
+
+io.sockets.on('connection', function(socket) {
+  readImg();
+  socket.emit("slideData", {
+    "dataImg": slidesObj,
+    "dir": slidesDir
+  });
+});
